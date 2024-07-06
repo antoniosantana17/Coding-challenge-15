20eade610d2afc20e7216922cdfe46e5d7fd475d
Antonio Santana <asantana35@usf.edu>
2024-07-05 20:54:30 -0400

    personalInfo.js
    "Add personal information section with styled components."

---
+++ PersonalInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './PersonalInfo.module.css';  // Import CSS module
+
+const PersonalInfo = () => (
+  <header className={styles.header}>
+    <h1>Antonio Santana</h1>
+    <p>Innovator by day, programmer by night. I am passionate about building scalable web applications and learning new technologies.</p>
+    <p>Email: tonys@gmail.com | LinkedIn: linkedin.com/in/tonys</p>
+  </header>
+);
+
+export default PersonalInfo;
